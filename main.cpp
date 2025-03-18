6dd64aba736b5a3e00550c6456ed926e51a43328
wistlov <wlovbukten@gmail.com>
2025-03-18 13:10:48 +0100

    Endret hello world til ant

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 // C++ programs start by executing the function main
 int main() {
     // Show a nice message in the Terminal window
-    cout << "Hello, World!" << endl;
+    cout << "Hello, Ant!" << endl;
 
     // This lets the operating system (Windows, Mac, Linux, etc.) know that the program
     // did not encounter any errors
