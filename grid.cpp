c05564374e483a876f73fc1490334186f41cfabb
wistlov <wlovbukten@gmail.com>
2025-03-18 14:45:56 +0100

    Fixed pathing for builddir

--- grid.cpp
+++ grid.cpp
@@ -5,6 +5,7 @@
 // Grid 15X15
 void Grid(int grid_width, int grid_height, int WIDTH, int HEIGHT, AnimationWindow& window) 
 { //grid_width and grid_height is the number of squares. WIDTH and HEIGHT is the window sizes
+    
    //Draw checkboard background
    int square_size = 50; //Size of squres on board
    for (int i = 0; i < WIDTH; i+=square_size) {
